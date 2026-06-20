e287d2a85e000a525c9a170aa342b161ea76820e
Pixtic <henryteirs@gmail.com>
2026-06-20 11:30:00 +0900

    Update system.js

--- system.js
+++ system.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let isGameActive = false;
 
     const difficulties = {
-        easy: { rows: 8, cols: 8, mines: 1 },
+        easy: { rows: 8, cols: 8, mines: 10 },
         medium: { rows: 16, cols: 16, mines: 40 },
         hard: { rows: 16, cols: 30, mines: 99 }
     };
